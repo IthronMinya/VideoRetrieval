6a2777f2f3fe31644deeb43e791ce972a8e3ebce
Michael Stroh <81415364+IthronMinya@users.noreply.github.com>
2023-10-12 19:27:08 +0200

    docker

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM  node:19 as build
+
+RUN apt-get update || : && apt-get -y install python3-pip
+
+ADD ./requirements.txt .
+
+ADD ./main.py .
+
+ADD ./frontend ./frontend
+
+RUN pip install --no-cache-dir --upgrade -r ./requirements.txt
+
+COPY . .
+ 
+WORKDIR /frontend
+
+RUN npm install
+
+RUN npm run build
+
+WORKDIR /
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--reload"]
+
--- main.py
+++ main.py
@@ -69,13 +69,8 @@ app.mount("/assets", StaticFiles(directory="public/assets"), name="static")
 def main():
     return "public/index.html"
 
-if __name__ == "__main__" and len(sys.argv) > 1:
-    match sys.argv[1]:
-        case 'dev' | "--dev" | "-d":
-            print("dev")
-            uvicorn.run("main:app", port=8002, reload=True)
-        case _:
-            uvicorn.run("main:app", port=8002)
+#if __name__ == "__main__":
+#    uvicorn.run("main:app", port=8002, reload=True)
 
 def cosine_sim(f1, f2):
 
